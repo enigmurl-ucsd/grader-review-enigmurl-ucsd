2a052d5ff40a6c30100270e8c36f3c98c2b54612
enigmurl <43832426+enigmurl@users.noreply.github.com>
2023-02-15 12:00:43 -0800

    Finish grader

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -9,13 +9,44 @@ class IsMoon implements StringChecker {
   }
 }
 
+class StringExample implements StringChecker {
+    public boolean checkString(String s) {
+        return s.startsWith("Good");
+    }
+}
+
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
+ 
+    @Test
+    public void testFilter() {
+        ArrayList<String> x = new ArrayList<>();
+        x.add("Good1");
+        x.add("Good2");
+        assertEquals(x, ListExamples.filter(x, new StringExample()));
+    }
+    
+   @Test(timeout = 500)
+    public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+    @Test(timeout = 1000)
+    public void testMerge() {
+        ArrayList<String> x = new ArrayList<>();
+        ArrayList<String> y = new ArrayList<>();
+        y.add("Good1");
+        assertEquals(y, ListExamples.merge(x,y));
+    }
+
+    @Test(timeout = 1000)
+    public void testMergeGood() {
+        ArrayList<String> x = new ArrayList<>();
+        x.add("Good1");
+        ArrayList<String> y = new ArrayList<>();
+        assertEquals(x, ListExamples.merge(x,y));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,47 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone $1 student-submission >git_status.txt 2>&1 
+
+if [[ $? -ne 0 ]] 
+then
+    echo "FAIL: Could not clone!" $(<git_status.txt)
+    exit 1
+fi
+
+if ! [[ -f student-submission/ListExamples.java ]]
+then
+    echo 'FAIL: Could not find file "ListExamples.java"'
+    exit 1
+fi
+
+cp TestListExamples.java student-submission/TestListExamples.java
+if ! [[ -d student-submission/lib ]]
+then
+    mkdir student-submission/lib
+fi
+cp -r lib student-submission 
+
+cd student-submission
+
+javac -cp $CPATH *.java 2>compile_error.txt
+
+if [[ $? -ne 0 ]]
+then
+    echo "FAIL: Error trying to compile files:" $(<compile_error.txt)
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples >run_error.txt 2>&1
+
+if [[ $? -ne 0 ]]
+then
+    ERR=$(grep "Tests run: .*" run_error.txt)
+    echo "FAIL:" $ERR
+exit 1
+fi
+
+echo "SUCCESS: File passed all tests"
+exit 0
+
+
